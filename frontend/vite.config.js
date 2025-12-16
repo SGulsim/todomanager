24d127e17d262aa3f5f236ab9b9ec74733e63552
SGulsim <saralievagulsim@gmail.com>
2025-12-16 22:09:03 +0300

    fix 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/todomanager/' : '/',
   server: {
     port: 3000,
     proxy: {
